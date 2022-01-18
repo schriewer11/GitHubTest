19f76b72589decf40e3d0b00e5e7a80af3306b37
schriewer11 <schriewer11@yahoo.com>
2022-01-18 15:07:25 -0500

    Second Attempt

---
+++ app/src/main/java/com/githubtest/v32001/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.githubtest.v32001
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.githubtest.v32001.ui.theme.GitHubTestTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GitHubTestTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    GitHubTestTheme {
+        Greeting("Android")
+    }
+}
